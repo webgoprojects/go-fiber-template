551041a33d173b48837f0c05564ce9e1e2415a19
Merrill Lines <linesmerrill@gmail.com>
2025-06-19 09:59:45 -0700

    Add basic routing

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/linesmerrill/go-fiber-template/routes"
 )
 
 func main() {
 	app := fiber.New()
+
+	routes.RegisterHealthRoutes(app)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
